3d615403e15a89c0c59701023f9feb1d9b28b615
Ricardo <ricardo.cruz29@ua.pt>
2021-04-06 16:10:41 +0100

    First 2 exercises of Lab3

---
+++ Lab3/SampleLab1/src/test/java/SampleTest/SampleTest.java
@@ -0,0 +1,34 @@
+package SampleTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class SampleTest {
+    WebDriver browser = null;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "/home/ricardo/Desktop/TQS/TQS/Lab3/geckodriver");
+        this.browser = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        this.browser.close();
+    }
+
+    @Test
+    public void site_header_is_on_home_page() {
+        this.browser.get("https://www.saucelabs.com");
+        WebElement href = this.browser.findElement(By.xpath("//a[@href='https://accounts.saucelabs.com/']"));
+        assertTrue((href.isDisplayed()));
+
+    }
+}
