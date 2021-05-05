fd486612f4078b4f5a9bd5747112bad31969dbdd
Ricardo <ricardo.cruz29@ua.pt>
2021-05-05 11:18:10 +0100

    Update

---
+++ Lab7/ex2/src/test/resources/db/migration/V001__INIT.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tqs_employee
+(
+    id    BIGSERIAL PRIMARY KEY,
+    name  varchar(25) NOT NULL,
+    email varchar(25) NOT NULL,
+    unique (email)
+);
+
+insert into tqs_employee(name, email)
+values ('Pedro', 'pedro@something.com');
+insert into tqs_employee(name, email)
+values ('Jose', 'ze@something.com');
+insert into tqs_employee(name, email)
+values ('Diogo', 'di@something.com');
+insert into tqs_employee(name, email)
+values ('Armindo', 'armindo@something.com');
